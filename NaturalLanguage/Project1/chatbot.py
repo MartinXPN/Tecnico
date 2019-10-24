8a9ca573e5356064fe310c99f62f66d427bf0181
MartinXPN <mirakyanmartin@gmail.com>
2019-10-24 19:05:04 +0100

    Added chatbot implementation with TF-IDF distance and stemmed tokens

--- NaturalLanguage/Project1/chatbot.py
+++ NaturalLanguage/Project1/chatbot.py
@@ -4,13 +4,17 @@ from xml.etree import ElementTree
 import fire
 import numpy as np
 import pandas as pd
+import time
 import spacy
-from nltk.metrics.distance import jaccard_distance
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 from tqdm import tqdm
 
 # Please download spaCy for Portuguese before using
 # `python -m spacy download pt_core_news_sm`
 nlp = spacy.load('pt_core_news_sm')
+sno = SnowballStemmer('portuguese')
 
 
 def read_data(xml_file_path):
@@ -40,10 +44,10 @@ def get_filters(q: str):
         'tokenized': ' '.join([token.text.lower() for token in doc]),
         'annotated': ' '.join([token.text.lower() + ' ' + token.pos_ for token in doc]),
         'filtered': ' '.join([token.text.lower() for token in doc if token.is_alpha and not token.is_stop]),
-        'filter_annotated': ' '.join([
-            token.text.lower() + ' ' + token.pos_ for token in doc if token.is_alpha and not token.is_stop
-        ]),
+        'filter_annotated': ' '.join([token.text.lower() + ' ' + token.pos_ for token in doc if token.is_alpha and not token.is_stop]),
         'lemma_filtered': ' '.join([token.lemma_.lower() for token in doc if token.is_alpha and not token.is_stop]),
+        'stem_filtered': ' '.join([sno.stem(token.text.lower()) for token in doc if token.is_alpha and not token.is_stop]),
+        'stem': ' '.join([sno.stem(token.text.lower()) for token in doc]),
     }
 
 
@@ -58,7 +62,22 @@ def add_experimental_columns(data: pd.DataFrame):
     data['annotated'] = [item['annotated'] for item in filters_applied]
     data['filter_annotated'] = [item['filter_annotated'] for item in filters_applied]
     data['lemma_filtered'] = [item['lemma_filtered'] for item in filters_applied]
+    data['stem_filtered'] = [item['stem_filtered'] for item in filters_applied]
+    data['stem'] = [item['stem'] for item in filters_applied]
     return data
+
+
+cache = {}
+vectorizer = None
+
+
+def tfidf(a, b):
+    if a not in cache: cache[a] = vectorizer.transform([a])
+    if b not in cache: cache[b] = vectorizer.transform([b])
+    avec = cache[a]
+    bvec = cache[b]
+    cosine_similarities = linear_kernel(avec, bvec).flatten()
+    return 1 - cosine_similarities[0]
 
 
 def query(q: str, corpus: pd.DataFrame, distance, threshold=10., column='question'):
@@ -71,20 +90,30 @@ def query(q: str, corpus: pd.DataFrame, distance, threshold=10., column='questio
     return corpus.iloc[best_match]['answer_id'], distances[best_match]
 
 
-def main(corpus_xml_path: str = 'KB.xml', test_path: str = 'test.txt', output_path: str = 'resultados.txt'):
+def main(corpus_xml_path: str = 'KB.xml', test_path: str = 'test.txt', output_path: str = 'resultados.txt',
+         column='stem'):
+    start = time.time()
     data = read_data(xml_file_path=corpus_xml_path)
     data = add_experimental_columns(data)
 
+    global cache
+    global vectorizer
+    cache = {}
+    vectorizer = TfidfVectorizer()
+    vectorizer.fit(data[column].values)
+
     with open(test_path, 'r') as f, open(output_path, 'w') as out:
-        for q in f:
+        for q in tqdm(f):
             filters = get_filters(q)
-            ans = query(filters['filtered'],
+            ans = query(filters[column],
                         corpus=data,
-                        distance=lambda a, b: jaccard_distance(set(a.split()), set(b.split())),
-                        threshold=0.7,
-                        column='filtered')
+                        distance=tfidf,
+                        threshold=0.6,
+                        column=column)
 
             out.write(f'{ans[0]}\n')
+
+    print('Elapsed time:', time.time() - start)
 
 
 if __name__ == '__main__':
