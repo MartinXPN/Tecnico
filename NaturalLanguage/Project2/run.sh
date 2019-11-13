0e72c70247d682881cc558f2805d414694994d9c
MartinXPN <mirakyanmartin@gmail.com>
2019-11-13 12:21:15 +0000

    lemma2noun created

--- NaturalLanguage/Project2/run.sh
+++ NaturalLanguage/Project2/run.sh
@@ -1,11 +1,3 @@
 # Nouns
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > lemma2noun.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > lemma2noun.pdf
-
-# barco+N+ms
-python3 word2fst.py -s syms.txt barco+N+ms > barco.txt
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  barco.txt | fstarcsort > barco.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait barco.fst | dot -Tpdf  > barco.pdf
-
-fstcompose barco.fst lemma2noun.fst > barco2noun.fst # DOES NOT WORK
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait barco2noun.fst | dot -Tpdf  > barco2noun.pdf
