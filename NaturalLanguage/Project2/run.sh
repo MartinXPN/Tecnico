3ffcd9938f836cf24a7f2656306f17b07e3ec097
MartinXPN <mirakyanmartin@gmail.com>
2019-11-13 12:28:14 +0000

    added lemma2adverb

--- NaturalLanguage/Project2/run.sh
+++ NaturalLanguage/Project2/run.sh
@@ -1,3 +1,7 @@
 # Nouns
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > lemma2noun.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > lemma2noun.pdf
+
+# Adverbs
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adverb.txt | fstarcsort > lemma2adverb.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adverb.fst | dot -Tpdf  > lemma2adverb.pdf
