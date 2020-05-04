19aa02a90da561301f1d777dec2ec441d311e22d
MartinXPN <mirakyanmartin@gmail.com>
2020-05-04 14:31:13 +0100

    Added Advanced algorithms project

---
+++ AdvancedAlgorithms/main.cpp
@@ -0,0 +1,14 @@
+#pragma GCC optimize ("O1")
+#pragma GCC optimize ("O2")
+#pragma GCC optimize ("O3")
+#pragma GCC push_options
+#pragma GCC optimize ("unroll-loops")
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
