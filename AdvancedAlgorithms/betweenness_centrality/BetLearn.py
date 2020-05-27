0fa32619459c26321066b6bc9b2928019fc460f4
MartinXPN <mirakyanmartin@gmail.com>
2020-05-27 12:01:26 +0100

    use data generator instead of manually iterating over

--- AdvancedAlgorithms/betweenness_centrality/BetLearn.py
+++ AdvancedAlgorithms/betweenness_centrality/BetLearn.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-
-@author: fanchangjun
-"""
 import os
 import pickle as cp
 import random
 import sys
 import time
+from typing import List
 
 import networkx as nx
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Lambda, Concatenate, Dense, LeakyReLU
-from tensorflow.keras.models import Model
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.utils import Sequence
 from tensorflow.python.keras.losses import LossFunctionWrapper
 from tqdm import tqdm
 
@@ -99,131 +95,128 @@ def create_drbc_model():
     return Model(inputs=[input_node_features, input_aux_features, input_n2n], outputs=out, name='DrBC')
 
 
+class DataGenerator(Sequence):
+    def __init__(self, graph_type: str, min_nodes: int, max_nodes: int, nb_graphs: int, graphs_per_batch: int, nb_batches: int, include_idx_map: bool = False, random_samples: bool = True, log_betweenness: bool = True):
+        self.utils = utils.py_Utils()
+        self.graphs = graph.py_GSet()
+        self.count: int = 0
+        self.betweenness: List[float] = []
+        self.graph_type: str = graph_type
+        self.min_nodes: int = min_nodes
+        self.max_nodes: int = max_nodes
+        self.nb_graphs: int = nb_graphs
+        self.graphs_per_batch: int = graphs_per_batch
+        self.nb_batches: int = nb_batches
+        self.include_idx_map: bool = include_idx_map
+        self.random_samples: bool = random_samples
+        self.log_betweenness: bool = log_betweenness
+
+        self.gen_new_graphs()
+
+    def __len__(self) -> int:
+        return self.nb_batches
+
+    def get_batch(self, graphs, ids: List[int]):
+        label = []
+        for i in ids:
+            label += self.betweenness[i]
+        label = np.array(label)
+
+        batch_graph = PrepareBatchGraph.py_PrepareBatchGraph(aggregatorID)
+        batch_graph.SetupBatchGraph(graphs)
+        assert (len(batch_graph.pair_ids_src) == len(batch_graph.pair_ids_tgt))
+
+        batch_size = len(label)
+        x = [
+            np.array(batch_graph.node_feat),
+            np.array(batch_graph.aux_feat),
+            batch_graph.n2nsum_param
+        ]
+        y = np.concatenate([
+            np.reshape(label, (batch_size, 1)),
+            np.reshape(batch_graph.pair_ids_src, (batch_size, -1)),
+            np.reshape(batch_graph.pair_ids_tgt, (batch_size, -1)),
+        ], axis=-1)
+        return (x, y, batch_graph.idx_map_list[0]) if self.include_idx_map else (x, y)
+
+    def __getitem__(self, index: int):
+        if self.random_samples:
+            g_list, id_list = self.graphs.Sample_Batch(self.graphs_per_batch)
+            return self.get_batch(graphs=g_list, ids=id_list)
+        return self.get_batch(graphs=[self.graphs.Get(index)], ids=[index])
+
+    def on_epoch_end(self):
+        self.clear()
+        self.gen_new_graphs()
+
+    @staticmethod
+    def gen_network(g):  # networkx2four
+        edges = g.edges()
+        if len(edges) > 0:
+            a, b = zip(*edges)
+            a = np.array(a)
+            b = np.array(b)
+        else:
+            a = np.array([0])
+            b = np.array([0])
+        return graph.py_Graph(len(g.nodes()), len(edges), a, b)
+
+    def gen_graph(self):
+        cur_n = np.random.randint(self.min_nodes, self.max_nodes)
+        if self.graph_type == 'erdos_renyi':        return nx.erdos_renyi_graph(n=cur_n, p=0.15)
+        elif self.graph_type == 'small-world':      return nx.connected_watts_strogatz_graph(n=cur_n, k=8, p=0.1)
+        elif self.graph_type == 'barabasi_albert':  return nx.barabasi_albert_graph(n=cur_n, m=4)
+        elif self.graph_type == 'powerlaw':         return nx.powerlaw_cluster_graph(n=cur_n, m=4, p=0.05)
+        raise ValueError(f'{self.graph_type} graph type is not supported yet')
+
+    def gen_new_graphs(self):
+        self.clear()
+        for _ in tqdm(range(self.nb_graphs), desc='generating new graphs...'):
+            g = self.gen_graph()
+            t = self.count
+            self.count += 1
+            self.graphs.InsertGraph(t, self.gen_network(g))
+
+            bc = self.utils.Betweenness(self.gen_network(g))
+            bc_log = self.utils.bc_log
+            self.betweenness.append(bc_log if self.log_betweenness else bc)
+
+    def clear(self):
+        self.count = 0
+        self.graphs.Clear()
+        self.betweenness = []
+
+
 class BetLearn:
 
     def __init__(self):
         # init some parameters
         self.g_type = 'powerlaw'  # 'erdos_renyi', 'powerlaw', 'small-world', 'barabasi_albert'
-        self.embedding_size = EMBEDDING_SIZE
-        self.learning_rate = LEARNING_RATE
-        self.reg_hidden = REG_HIDDEN
-        self.TrainSet = graph.py_GSet()
-        self.TestSet = graph.py_GSet()
-        self.utils = utils.py_Utils()
-        self.TrainBetwList = []
-        self.TestBetwList = []
-        self.ngraph_train = 0
-        self.ngraph_test = 0
-
         self.metrics = metrics.py_Metrics()
-        self.inputs = dict()
 
         self.model = create_drbc_model()
         self.model.summary()
         self.model.compile(optimizer='adam',
                            loss=LossFunctionWrapper(pairwise_ranking_crossentropy_loss, reduction='none'))
 
-    def gen_graph(self, num_min, num_max):
-        cur_n = np.random.randint(num_max - num_min + 1) + num_min
-        if self.g_type == 'erdos_renyi':
-            g = nx.erdos_renyi_graph(n=cur_n, p=0.15)
-        elif self.g_type == 'small-world':
-            g = nx.connected_watts_strogatz_graph(n=cur_n, k=8, p=0.1)
-        elif self.g_type == 'barabasi_albert':
-            g = nx.barabasi_albert_graph(n=cur_n, m=4)
-        elif self.g_type == 'powerlaw':
-            g = nx.powerlaw_cluster_graph(n=cur_n, m=4, p=0.05)
-        return g
+        self.train_generator = DataGenerator(graph_type=self.g_type, min_nodes=NUM_MIN, max_nodes=NUM_MAX, nb_graphs=n_train, graphs_per_batch=BATCH_SIZE, nb_batches=5000, include_idx_map=False, random_samples=True, log_betweenness=True)
+        self.valid_generator = DataGenerator(graph_type=self.g_type, min_nodes=NUM_MIN, max_nodes=NUM_MAX, nb_graphs=n_valid, graphs_per_batch=BATCH_SIZE, nb_batches=1, include_idx_map=True, random_samples=False, log_betweenness=False)
 
-    def gen_new_graphs(self, num_min, num_max):
-        print('\ngenerating new training graphs...')
-        self.ClearTrainGraphs()
-        for i in tqdm(range(n_train)):
-            g = self.gen_graph(num_min, num_max)
-            self.InsertGraph(g, is_test=False)
-            bc = self.utils.Betweenness(self.GenNetwork(g))
-            bc_log = self.utils.bc_log
-            self.TrainBetwList.append(bc_log)
+    def Predict(self, gid):
+        x, y, idx_map = self.valid_generator[gid]
+        result = self.model.predict_on_batch(x=x).flatten()
 
-    def ClearTrainGraphs(self):
-        self.ngraph_train = 0
-        self.TrainSet.Clear()
-        self.TrainBetwList = []
-        self.TrainBetwRankList = []
-
-    def ClearTestGraphs(self):
-        self.ngraph_test = 0
-        self.TestSet.Clear()
-        self.TestBetwList = []
-
-    def InsertGraph(self, g, is_test):
-        if is_test:
-            t = self.ngraph_test
-            self.ngraph_test += 1
-            self.TestSet.InsertGraph(t, self.GenNetwork(g))
-        else:
-            t = self.ngraph_train
-            self.ngraph_train += 1
-            self.TrainSet.InsertGraph(t, self.GenNetwork(g))
-
-    def PrepareValidData(self):
-        print('\ngenerating validation graphs...')
-        sys.stdout.flush()
-        self.ClearTestGraphs()
-        for i in tqdm(range(n_valid)):
-            g = self.gen_graph(NUM_MIN, NUM_MAX)
-            self.InsertGraph(g, is_test=True)
-            bc = self.utils.Betweenness(self.GenNetwork(g))
-            self.TestBetwList.append(bc)
-
-    def SetupBatchGraph(self, g_list):
-        prepareBatchGraph = PrepareBatchGraph.py_PrepareBatchGraph(aggregatorID)
-        prepareBatchGraph.SetupBatchGraph(g_list)
-        self.inputs['n2nsum_param'] = prepareBatchGraph.n2nsum_param
-        self.inputs['node_feat'] = np.array(prepareBatchGraph.node_feat)
-        self.inputs['aux_feat'] = np.array(prepareBatchGraph.aux_feat)
-        self.inputs['pair_ids_src'] = np.array(prepareBatchGraph.pair_ids_src)
-        self.inputs['pair_ids_tgt'] = np.array(prepareBatchGraph.pair_ids_tgt)
-        assert (len(prepareBatchGraph.pair_ids_src) == len(prepareBatchGraph.pair_ids_tgt))
-        return prepareBatchGraph.idx_map_list
-
-    def Predict(self, g_list):
-        idx_map_list = self.SetupBatchGraph(g_list)
-        result = self.model.predict_on_batch(x=(self.inputs['node_feat'],
-                                                self.inputs['aux_feat'],
-                                                self.inputs['n2nsum_param']))
-        result = result.flatten()
-
-        idx_map = idx_map_list[0]
         # idx_map[i] >= 0:  # corresponds to nodes with 0.0 betw_log value
-        result_output = [np.power(10, -pred_betweenness) if idx_map[i] >= 0 else 0 for i, pred_betweenness in enumerate(result)]
+        result_output = [np.power(10, -pred_betweenness) if idx_map[i] >= 0 else 0
+                         for i, pred_betweenness in enumerate(result)]
         return result_output
 
     def Fit(self):
-        g_list, id_list = self.TrainSet.Sample_Batch(BATCH_SIZE)
-
-        Betw_Label_List = []
-        for id in id_list:
-            Betw_Label_List += self.TrainBetwList[id]
-        label = np.array(Betw_Label_List)
-        self.inputs['label'] = label
-        self.SetupBatchGraph(g_list)
-
-        batch_size = len(self.inputs['label'])
-        y_true = np.concatenate([
-            np.reshape(self.inputs['label'], (batch_size, 1)),
-            np.reshape(self.inputs['pair_ids_src'], (batch_size, -1)),
-            np.reshape(self.inputs['pair_ids_tgt'], (batch_size, -1)),
-        ], axis=-1)
-
-        loss = self.model.train_on_batch(x=[self.inputs['node_feat'], self.inputs['aux_feat'], self.inputs['n2nsum_param']],
-                                         y=y_true)
-        return loss / len(g_list)
+        x, y = self.train_generator[0]  # next batch
+        loss = self.model.train_on_batch(x=x, y=y)
+        return loss
 
     def Train(self):
-        self.PrepareValidData()
-        self.gen_new_graphs(NUM_MIN, NUM_MAX)
-
         save_dir = './models'
         VCFile = '%s/ValidValue.csv' % (save_dir)
         f_out = open(VCFile, 'w')
@@ -231,7 +224,8 @@ class BetLearn:
             TrainLoss = self.Fit()
             start = time.clock()
             if iter and iter % 5000 == 0:
-                self.gen_new_graphs(NUM_MIN, NUM_MAX)
+                self.train_generator.on_epoch_end()
+                self.valid_generator.on_epoch_end()
             if iter % 500 == 0:
                 if (iter == 0):
                     N_start = start
@@ -257,40 +251,20 @@ class BetLearn:
         f_out.close()
 
     def Test(self, gid):
-        g_list = [self.TestSet.Get(gid)]
         start = time.time()
-        betw_predict = self.Predict(g_list)
+        betw_predict = self.Predict(gid)
         end = time.time()
-        betw_label = self.TestBetwList[gid]
+        betw_label = self.valid_generator.betweenness[gid]
 
         run_time = end - start
         topk = self.metrics.RankTopK(betw_label, betw_predict, 0.01)
         kendal = self.metrics.RankKendal(betw_label, betw_predict)
         return run_time, topk, kendal
 
-    def findModel(self):
-        VCFile = './models/ValidValue.csv'
-        vc_list = []
-        EarlyStop_start = 2
-        EarlyStop_length = 1
-        num_line = 0
-        for line in open(VCFile):
-            data = float(line.split(',')[0].strip(','))  # 0:topK; 1:kendal
-            vc_list.append(data)
-            num_line += 1
-            if num_line > EarlyStop_start and data < np.mean(vc_list[-(EarlyStop_length + 1):-1]):
-                best_vc = num_line
-                break
-        best_model_iter = 500 * best_vc
-        best_model = './models/nrange_iter_%d.ckpt' % (best_model_iter)
-        return best_model
-
-    def EvaluateSynData(self, data_test, model_file=None):  # test synthetic data
-        if model_file == None:  # if user do not specify the model_file
-            model_file = self.findModel()
+    def EvaluateSynData(self, data_test, model_file):  # test synthetic data
         print('The best model is :%s' % (model_file))
         sys.stdout.flush()
-        self.LoadModel(model_file)
+        load_model(model_file)
         n_test = 100
         frac_run_time, frac_topk, frac_kendal = 0.0, 0.0, 0.0
         self.ClearTestGraphs()
@@ -311,7 +285,7 @@ class BetLearn:
     def EvaluateRealData(self, model_file, data_test, label_file):  # test real data
         g = nx.read_weighted_edgelist(data_test)
         sys.stdout.flush()
-        self.LoadModel(model_file)
+        load_model(model_file)
         betw_label = []
         for line in open(label_file):
             betw_label.append(float(line.strip().split()[1]))
@@ -332,14 +306,3 @@ class BetLearn:
         kendal = self.metrics.RankKendal(betw_label, betw_predict)
         self.ClearTestGraphs()
         return top001, top005, top01, kendal, run_time
-
-    def GenNetwork(self, g):  # networkx2four
-        edges = g.edges()
-        if len(edges) > 0:
-            a, b = zip(*edges)
-            A = np.array(a)
-            B = np.array(b)
-        else:
-            A = np.array([0])
-            B = np.array([0])
-        return graph.py_Graph(len(g.nodes()), len(edges), A, B)
