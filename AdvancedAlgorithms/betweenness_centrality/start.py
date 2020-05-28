8f249808d26b403187a00fcde0b4cb3cfaa0fe45
MartinXPN <mirakyanmartin@gmail.com>
2020-05-28 13:29:40 +0100

    set parameters to be the same as in the original paper

--- AdvancedAlgorithms/betweenness_centrality/start.py
+++ AdvancedAlgorithms/betweenness_centrality/start.py
@@ -7,8 +7,8 @@ def main():
     # from tensorflow.keras.models import load_model
     # load_model('path/to/experiments/<DATE>/models/best.h5py', custom_objects={'tf': tf}, compile=False).summary()
 
-    btl = BetLearn(min_nodes=50, max_nodes=100, nb_train_graphs=1000, nb_valid_graphs=100, graphs_per_batch=16, nb_batches=50,
-                   graph_type='powerlaw', optimizer='adam', aggregation='max', combine='gru')
+    btl = BetLearn(min_nodes=4000, max_nodes=5000, nb_train_graphs=1000, nb_valid_graphs=100, graphs_per_batch=16, nb_batches=500,
+                   graph_type='powerlaw', optimizer='adam', aggregation='lstm', combine='gru')
     btl.train(epochs=10000)
 
 
